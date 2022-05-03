2c35ddd310e576d8638775962eb7a7dcead3dfc0
Jan <33833587+Model2TypeE@users.noreply.github.com>
2022-05-03 06:21:36 +0200

    Initial project setup

---
+++ VulkanEngine/Vermillion/src/entry.cpp
@@ -0,0 +1,14 @@
+#include "pch.hpp"
+
+int main() {
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    return 0;
+}
---
+++ VulkanEngine/Vermillion/src/pch/pch.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <optional>
+#include <set>
+#include <cstdint>
+#include <algorithm>
+#include <fstream>
+
+// Enable the WSI extensions
+#if defined(_WIN32)
+#define VK_USE_PLATFORM_WIN32_KHR
+#endif
+
+// Tell SDL not to mess with main()
+#define SDL_MAIN_HANDLED
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+#include <SDL2/SDL_vulkan.h>
+
+#define VULKAN_HPP_NO_SPACESHIP_OPERATOR
+#include <vulkan/vulkan.hpp>
