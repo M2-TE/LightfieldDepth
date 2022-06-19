76bc4019f3f70d1f5f734a0e83f8985a65fcf565
Jan <33833587+M2-TE@users.noreply.github.com>
2022-06-19 04:21:56 +0200

    linux-specific setups cleaned up, shader wrapper moved into verm

--- VulkanEngine/Shaders/include/shaders.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct ShaderData { const unsigned char* pData; size_t size; };
-struct ShaderPack { ShaderData vs, ps; };
-
-extern const ShaderPack geometryPass, lightingPass;
-extern const ShaderPack swapchainWrite;
--- VulkanEngine/Vermillion/src/core/wrappers/shader_wrapper.hpp
+++ VulkanEngine/Vermillion/src/core/wrappers/shader_wrapper.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
-#include "shaders.hpp"
+#include "headers/lighting_pass.vs.hpp"
+#include "headers/lighting_pass.ps.hpp"
+#include "headers/geometry_pass.vs.hpp"
+#include "headers/geometry_pass.ps.hpp"
+#include "headers/swapchain_write.vs.hpp"
+#include "headers/swapchain_write.ps.hpp"
+
+struct ShaderData { const unsigned char* pData; size_t size; };
+struct ShaderPack { ShaderData vs, ps; };
+const ShaderPack geometryPass = { { geometry_pass_vs, sizeof(geometry_pass_vs) }, { geometry_pass_ps, sizeof(geometry_pass_ps) } };
+const ShaderPack lightingPass = { { lighting_pass_vs, sizeof(lighting_pass_vs) }, { lighting_pass_ps, sizeof(lighting_pass_ps) } };
+const ShaderPack swapchainWrite = { { swapchain_write_vs, sizeof(swapchain_write_vs) }, { swapchain_write_ps, sizeof(swapchain_write_ps) } };
 
 vk::ShaderModule create_shader_module(DeviceWrapper& deviceWrapper, const unsigned char* data, size_t size)
 {
