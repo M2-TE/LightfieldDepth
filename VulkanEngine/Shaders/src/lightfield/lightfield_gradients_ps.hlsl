9767db9c4ac7d5a185901e05b6a2d4375227fcc2
Jan <33833587+M2-TE@users.noreply.github.com>
2022-08-25 12:41:50 +0200

    5-/7-/9-tap filter for varying patch size, confidence estimator

--- VulkanEngine/Shaders/src/lightfield/lightfield_gradients_ps.hlsl
+++ VulkanEngine/Shaders/src/lightfield/lightfield_gradients_ps.hlsl
@@ -1,6 +1,9 @@
 #define BRIGHTNESS(col) dot(col, float3(0.333333f, 0.333333f, 0.333333f)); // using standard greyscale
 #define BRIGHTNESS_REAL(col) dot(col, float3(0.299f, 0.587f, 0.114f)); // using luminance construction
 
+// TODO: squared error?
+// mip-map/pyramid
+
 Texture2DArray colBuffArr : register(t0);
 
 float4 main(float4 screenPos : SV_Position) : SV_Target
@@ -14,34 +17,39 @@ float4 main(float4 screenPos : SV_Position) : SV_Target
     float Lv = 0.0f;
 
     // derivative approximation filters
-    const float3 p = float3(0.229879f, 0.540242f, 0.229879f);
-    const float3 d = float3(-0.425287f, 0.0f, 0.425287f);
-
-    // TODO: filter set for patch(x,y)
-    // TODO: second set of loops
-    // TODO: squared error?
-    // mip-map/pyramid
+    float p_tap3[] = {  0.229879f, 0.540242f, 0.229879f };
+    float d_tap3[] = { -0.425287f, 0.000000f, 0.425287f };
+    float p_tap5[] = {  0.037659f,  0.249153f, 0.426375f, 0.249153f, 0.037659f };
+    float d_tap5[] = { -0.109604f, -0.276691f, 0.000000f, 0.276691f, 0.109604f };
+    float p_tap7[] = {  0.004711f,  0.069321f,  0.245410f, 0.361117f, 0.245410f, 0.069321f, 0.004711f };
+    float d_tap7[] = { -0.018708f, -0.125376f, -0.193091f, 0.000000f, 0.193091f, 0.125376f, 0.018708f };
+    float p_tap9[] = {  0.000721f,  0.015486f,  0.090341f,  0.234494f, 0.317916f, 0.234494f, 0.090341f, 0.015486f, 0.000721f };
+    float d_tap9[] = { -0.003059f, -0.035187f, -0.118739f, -0.143928f, 0.000000f, 0.143928f, 0.118739f, 0.035187f, 0.003059f };
+    
+    int nCams = 3; // cams in one dimension
+    int nPixels = 3; // pixels in one dimension
+    int pixelOffset = nPixels / 2;
     
     // iterate over 2D patch of pixels (3x3)
-    for (int x = 0; x < 3; x++)
+    for (int x = 0; x < nPixels; x++)
     {
-        for (int y = 0; y < 3; y++)
+        for (int y = 0; y < nPixels; y++)
         {
-            for (int u = 0; u < 3; u++)
+            for (int u = 0; u < nCams; u++)
             {
-                for (int v = 0; v < 3; v++)
+                for (int v = 0; v < nCams; v++)
                 {
                     int camIndex = u * 3 + v;
-                    int3 texOffset = int3(x - 1, y - 1, camIndex);
+                    int3 texOffset = int3(x - pixelOffset, y - pixelOffset, camIndex);
 
                     float3 color = colBuffArr[uint3(texPos + texOffset)].rgb;
                     float luma = BRIGHTNESS(color);
-
+                    
                     // approximate derivatives using 3-tap filter
-                    Lx += d[x] * p[y] * p[u] * p[v] * luma;
-                    Ly += p[x] * d[y] * p[u] * p[v] * luma;
-                    Lu += p[x] * p[y] * d[u] * p[v] * luma;
-                    Lv += p[x] * p[y] * p[u] * d[v] * luma;
+                    Lx += d_tap3[x] * p_tap3[y] * p_tap3[u] * p_tap3[v] * luma;
+                    Ly += p_tap3[x] * d_tap3[y] * p_tap3[u] * p_tap3[v] * luma;
+                    Lu += p_tap3[x] * p_tap3[y] * d_tap3[u] * p_tap3[v] * luma;
+                    Lv += p_tap3[x] * p_tap3[y] * p_tap3[u] * d_tap3[v] * luma;
                 }
             }
         }
